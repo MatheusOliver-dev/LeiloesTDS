287a007cbc680918672728574b00c4d067f09824
Matheus Oliver <matheusfpereira123@gmail.com>
2024-10-27 22:35:23 -0300

    Definição da tela inicial

---
+++ src/Principal.java
@@ -0,0 +1,10 @@
+
+import VIEW.cadastroVIEW;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        cadastroVIEW telaPrincipal = new cadastroVIEW();
+        telaPrincipal.setVisible(true);
+    }
+}
