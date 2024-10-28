9a105c7a51b7694f0cac6b19ba3368ce3f2d4fb1
Matheus Oliver <matheusfpereira123@gmail.com>
2024-10-27 22:11:39 -0300

    Remoção de comentários

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,19 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
 
 public class ProdutosDAO {
     
@@ -34,9 +23,5 @@ public class ProdutosDAO {
         
         return listagem;
     }
-    
-    
-    
-        
+ 
 }
-
