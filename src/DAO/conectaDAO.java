8778e2ef6bf5ba8b4af6d3f909ed0c45dca86449
Matheus Oliver <matheusfpereira123@gmail.com>
2024-10-27 23:39:32 -0300

    Alterada as credenciais de conexão MySQL

--- src/DAO/conectaDAO.java
+++ src/DAO/conectaDAO.java
@@ -12,7 +12,7 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=matheus123&useSSL=false");
             
         } catch (SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
