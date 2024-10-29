a22f231e5f200dc5cd964f03d848549bd75b6c21
Matheus Oliver <matheusfpereira123@gmail.com>
2024-10-28 22:39:43 -0300

    Implementação da tela de vendas

--- src/Principal.java
+++ src/Principal.java
@@ -1,10 +1,14 @@
 
 import VIEW.cadastroVIEW;
+import VIEW.vendasVIEW;
 
 public class Principal {
 
     public static void main(String[] args) {
         cadastroVIEW telaPrincipal = new cadastroVIEW();
         telaPrincipal.setVisible(true);
+        
+        vendasVIEW vendas = new vendasVIEW();
+        vendas.setVisible(true);
     }
 }
