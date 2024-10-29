a860e165524a9e7159e7a3624adb8b426197a8b4
Matheus Oliver <matheusfpereira123@gmail.com>
2024-10-28 22:44:06 -0300

    Implementação da funcionalidade da venda de produtos

--- src/Principal.java
+++ src/Principal.java
@@ -1,14 +1,10 @@
 
 import VIEW.cadastroVIEW;
-import VIEW.vendasVIEW;
 
 public class Principal {
 
     public static void main(String[] args) {
         cadastroVIEW telaPrincipal = new cadastroVIEW();
         telaPrincipal.setVisible(true);
-        
-        vendasVIEW vendas = new vendasVIEW();
-        vendas.setVisible(true);
     }
 }
